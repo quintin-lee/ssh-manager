3f034a0b74046f75f4b143488ee11aff45329fa4
quintin <2449164582@qq.com>
2026-07-13 09:41:19 +0800

    remove C version: delete src/, obj/, Makefile, sshm binary, compile_commands.json
    Keep shell version (bin/sshm.sh) with all tests, packaging, and CI intact.

--- src/import_export.h
+++
@@ -1,8 +0,0 @@
-#ifndef IMPORT_EXPORT_H
-#define IMPORT_EXPORT_H
-
-int export_config_interactive(void);
-int import_config_interactive(void);
-int export_ssh_config(void);
-
-#endif
--- src/ssh.h
+++
@@ -1,7 +0,0 @@
-#ifndef SSH_H
-#define SSH_H
-
-#include "yaml_parser.h"
-int ssh_connect(const node_t *node);
-
-#endif
